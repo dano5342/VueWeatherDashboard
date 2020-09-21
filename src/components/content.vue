37f016196017b1a19086d358928a296c27d67999
dano5342 <ellis.daniel0@gmail.com>
2020-09-21 12:40:09 +0200

    removed sass cache

---
+++ src/components/content.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p> This child components of content.vue are:</p>
+        <ul>
+            <li v-for="child in childComponents"> {{ child }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            childComponents: ['temp_var_chart.vue', 'highlights.vue']
+        }
+    },
+
+    methods: {},
+    computed: {},
+    
+
+}
+</script>
