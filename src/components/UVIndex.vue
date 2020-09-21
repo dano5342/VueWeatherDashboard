3c24a59dcb61b493622e2814c4e2481dcf3db946
dano5342 <ellis.daniel0@gmail.com>
2020-09-21 15:02:31 +0200

    fixed typo for prop

--- src/components/UVIndex.vue
+++ src/components/UVIndex.vue
@@ -14,7 +14,7 @@ export default {
     },
     data() {
         return {
-            uvindex: this.highlights.uvindex
+            uvindex: this.highlights.uvIndex
         }
     },
     methods: {
