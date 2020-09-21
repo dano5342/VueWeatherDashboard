7e377fbf5f361a317bbdba615c4b387f7b93e2fe
dano5342 <ellis.daniel0@gmail.com>
2020-09-21 11:55:04 +0200

    added some SVG's from the tutorial. Styling started, creating new branch

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import FusionCharts from 'fusioncharts';
+import Charts from 'fusioncharts/fusioncharts.charts';
+import Widgets from 'fusioncharts/fusioncharts.widgets';
+import PowerCharts from 'fusioncharts/fusioncharts.powercharts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+import VueFusionCharts from 'vue-fusioncharts';
 
+
+// Resolution
+Charts(FusionCharts);
+PowerCharts(FusionCharts);
+Widgets(FusionCharts);
+FusionTheme(FusionCharts);
+
+// Global register for component usage
+Vue.use(VueFusionCharts, FusionCharts)
+
+
+// App Setup
 new Vue({
   el: '#app',
   render: h => h(App)
